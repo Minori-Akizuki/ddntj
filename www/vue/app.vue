a3ea81852038c75143db49bb0f70f59c5f35bb1f
c6h4clch3 <c6h4clch3@gmail.com>
2017-08-23 14:00:13 +0900

    refactor: [フロントエンド]アプリケーションをVueコンポーネントに

---
+++ www/vue/app.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <chatbox :yourname="yourname" :systems="systems"
+             :selected="selectedSystem" v-on:systemChanged="changeSystem"></chatbox>
+  </div>
+</template>
+<script>
+import chatbox from './components/chatbox/chatbox';
+import dicebot from '../js/dicebot';
+import io from 'socket.io-client';
+
+const serverUrl = location.href;
+const socketio = io.connect(serverUrl);
+
+export default {
+  data() {
+    return {
+      yourname: '',
+      systems: [],
+      selectedSystem: 'DiceBot'
+    }
+  },
+  components: {
+    chatbox
+  },
+  methods: {
+    initName: function() {
+      const defname = Math.floor(Math.random()*100) + "さん";
+      this.yourname = defname
+      socketio.emit('connected', defname);
+    },
+    changeSystem: function(newSystem) {
+      this.selectedSystem = newSystem;
+    }
+  },
+  created: function() {
+    dicebot.getsystems((systems) => {
+      this.systems = systems;
+    });
+    this.initName();
+  }
+}
+</script>
+<style>
+
+</style>
+
