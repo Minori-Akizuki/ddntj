8d41e6db7722d3bcfcd8ed28f157618da5159e4d
c6h4clch3 <c6h4clch3@gmail.com>
2017-08-29 14:07:38 +0900

    modify: APIはHTTPS読み込み

--- www/js/dicebot.js
+++ www/js/dicebot.js
@@ -6,7 +6,7 @@ dicebot.systems = [];
 dicebot.getsystems = function(callback){
   if(!dicebot.systems.length){
     $.getJSON(
-      'http://www2.taruki.com/bcdice-api/v1/systems?callback=?'
+      'https://www2.taruki.com/bcdice-api/v1/systems?callback=?'
     ).done(
       function(data){
         dicebot.systems = data.systems;
