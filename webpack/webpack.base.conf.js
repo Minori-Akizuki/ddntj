b339cc79af493471f93688a1bb2e217f682dc35a
Minori-Akizuki <dreamnstd@gmail.com>
2019-11-08 10:33:06 +0900

    add : bootstrapを導入

--- webpack/webpack.base.conf.js
+++ webpack/webpack.base.conf.js
@@ -23,6 +23,10 @@ module.exports = {
         use: [
           'babel-loader'
         ]
+      },
+      {
+        test: /\.css$/, 
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
--- www/js/ddntj.js
+++ www/js/ddntj.js
@@ -1,6 +1,11 @@
 'use strict';
 import Vue from 'vue';
 import app from '../vue/app';
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 // vue
 new Vue({
