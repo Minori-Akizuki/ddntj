346bf2bcc90e622c918336f252f0504815eca00b
Minori-Akizuki <dreamnstd@gmail.com>
2019-12-10 13:10:37 +0900

    スタブの作成

--- www/vue/components/menubar/menubar.vue
+++ www/vue/components/menubar/menubar.vue
@@ -1,11 +1,14 @@
 <template>
     <div>
         <div id="header">
-            <ul>
-                <li v-for="content in menu" v-bind:key="content.id">
-                    {{content.text}}
-                </li>
-            </ul>
+            <b-dropdown id="room" text="部屋" class="m-md-2 button" size="sm">
+                <b-dropdown-item disabled>未実装</b-dropdown-item>
+            </b-dropdown>
+            <b-dropdown id="resource" text="画像類" class="m-md-2 button" size="sm">
+                <b-dropdown-item>画像</b-dropdown-item>
+                <b-dropdown-item>音源</b-dropdown-item>
+                <b-dropdown-item>動画</b-dropdown-item>
+            </b-dropdown>
             <div id="title">どどんてぃ</div>
         </div>
     </div>
@@ -18,7 +21,7 @@ export default {
     return {
         menu : [
             {'id':1, 'text':'hogehoge'},
-            {'id':2, 'text':'hugahuga'}
+            {'id':2, 'text':'リソース'}
         ]
     };
   },
@@ -31,6 +34,17 @@ export default {
 }
 </script>
 <style>
-
+#header{
+    width: 100%;
+    height: 50px;
+}
+div.button{
+    float:  left;
+    vertical-align: middle;
+}
+#title{
+    height: 50px;
+    line-height: 40px;
+}
 </style>
 
